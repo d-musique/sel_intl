7fded2d9a3f25af13bcbdc6f98364934ab679969
D.musique <d-musique@users.noreply.github.com>
2024-10-09 01:45:29 +0200

    Add format_arg attributes

--- include/sel/intl.h
+++ include/sel/intl.h
@@ -11,12 +11,18 @@
 extern "C" {
 #endif
 
-const char *sel_gettext(const char *text);
-const char *sel_dgettext(const char *domain, const char *text);
-const char *sel_dcgettext(const char *domain, const char *text, int category);
-const char *sel_ngettext(const char *text, const char *plural, unsigned long n);
-const char *sel_dngettext(const char *domain, const char *text, const char *plural, unsigned long n);
-const char *sel_dcngettext(const char *domain, const char *text, const char *plural, unsigned long n, int category);
+#if defined(__GNUC__)
+#define SEL_INTL_FORMAT_ARG(n) __attribute__((format_arg(n)))
+#else
+#define SEL_INTL_FORMAT_ARG(n)
+#endif
+
+const char *sel_gettext(const char *text) SEL_INTL_FORMAT_ARG(1);
+const char *sel_dgettext(const char *domain, const char *text) SEL_INTL_FORMAT_ARG(2);
+const char *sel_dcgettext(const char *domain, const char *text, int category) SEL_INTL_FORMAT_ARG(2);
+const char *sel_ngettext(const char *text, const char *plural, unsigned long n) SEL_INTL_FORMAT_ARG(1) SEL_INTL_FORMAT_ARG(2);
+const char *sel_dngettext(const char *domain, const char *text, const char *plural, unsigned long n) SEL_INTL_FORMAT_ARG(2) SEL_INTL_FORMAT_ARG(3);
+const char *sel_dcngettext(const char *domain, const char *text, const char *plural, unsigned long n, int category) SEL_INTL_FORMAT_ARG(2) SEL_INTL_FORMAT_ARG(3);
 const char *sel_bindtextdomain(const char *domain, const char *dirname);
 const char *sel_textdomain(const char *domain);
 
